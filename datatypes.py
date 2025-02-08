02d72cf62c092313fed47fed115df23ba7229fdc
Ali Raza <120801089+infoman55@users.noreply.github.com>
2025-02-08 02:11:38 -0800

    firstcommit

---
+++ datatypes.py
@@ -0,0 +1,98 @@
+ # python .\datatypes.py (. is the current folder and /datatypes.py is the file )
+"""test='hello world' #array of charcter /list of chars"""
+# int
+"""
+x=45  
+y=43
+"""
+# string
+"""x="Ali Raza" """
+
+# float 
+"""z=2.5"""
+
+# boolean
+"""a=True 
+b=False
+
+result=x/y
+print(result,a)"""
+
+#Task in class
+#area of Triangle
+"""
+base=10 
+height=45
+area=(1/2)*base*height
+print("Area of the triangle:" ,area)"""
+
+
+#area of circle.
+
+"""
+Radius=5
+
+pi=3.14159
+
+Area=pi*Radius*Radius
+print("Area of the Circle: ",Area)
+"""
+
+#  list 
+"""students=['ali','usman','uzair']#list
+print(students)"""
+
+# tuple 
+ 
+# none 
+# undefine
+
+# dictionary
+"""student_1 = students[0]
+
+student_2 = students[1]
+
+student_3 = students[2]"""
+
+# concatenation
+"""test = 'hello '+'world'
+
+student_1["name"]
+student_1["age"]
+student_1["email"]
+
+print(student_1.name, student_1["email"], student_1["age"])
+
+student_1 = {
+    "name": "ali",
+    "age": 21,
+    "email": "test@example.com"
+}"""""
+
+#task2
+"""test = ['hello', 'world', 'this', 'is', 'me']"""
+
+#hint = [{'name': 'talha'}, ]
+
+# make a list of student objects
+"""students={
+    'name':'talha'
+}
+print(test,students['name'])"""
+
+# make a list of student object 
+
+"""Student=[
+    {"name":"Ali Raza" , "age":20 ,"Subject":"Python"},
+    {"name":"Usman" , "age":21,"Subject":"Python"},
+    {"name":"Uzair","age":21,"Subject":"C++"},
+    {"name":"Talha", "age":26,"Subject":"JavaScript"}
+]
+print(Student)
+#using dictionary OR Array 
+print(Student[0])
+print(Student[1])
+print(Student[2])
+print(Student[3])"""
+
+
