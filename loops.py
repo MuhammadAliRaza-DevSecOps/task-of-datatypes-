535dbb4aaf5eaf047c980b083f54d5fd0318e501
Ali Raza <120801089+infoman55@users.noreply.github.com>
2025-02-16 15:31:12 +0500

    this file is loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+#for loop only used in string or dictionary print each chaarcter .
+
+#print the sqaure of each number in the list 
+"""
+num=[1,2,3,4,5]
+for mul in num:
+    print(f"the list is ",{mul},"and square of list is ",{mul**2})
+"""
+# for making 100 time loop 
+"""
+for num in range(0,101): # use range keyword
+    print(num**2)
+"""
