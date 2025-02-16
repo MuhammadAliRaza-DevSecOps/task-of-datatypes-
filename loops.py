9288c8099d71c322becad7817f88dd071853c5e1
Ali Raza <120801089+infoman55@users.noreply.github.com>
2025-02-16 15:58:49 +0500

    this file is edited

--- loops.py
+++ loops.py
@@ -11,3 +11,49 @@ for mul in num:
 for num in range(0,101): # use range keyword
     print(num**2)
 """
+# while loop is used for more loop and also more iteration
+"""
+i=0
+while i<5:
+    i=i+1
+    #i+=1 both are same 
+    print(i)
+"""
+# print even number from 0 to 10
+"""
+i=0
+while i<=10:
+    i+=2
+    print(i)
+"""
+# break used for immdiate break the loop 
+"""
+for num in range(1, 6):
+    if num == 3:
+        print("Breaking the loop at", num)
+        break
+    print(num)
+"""
+# continue used skips the current iteration of the loop
+"""
+for num in range(1, 6):
+    if num == 3:
+        print("Skipping", num)
+        continue
+    print(num)
+"""
+# game to guess the value 
+import random 
+secret_number=random.randint(1,10)
+guess=0
+while not(guess== secret_number):
+    guess = int(input("Guess the number:"))
+    if guess<secret_number:
+        print("The numnber is to low ")
+    elif guess>secret_number:
+        print("The number is to high")
+    else:
+        print("Correct")
+
+#OOP
+#four pilers of OOP 
